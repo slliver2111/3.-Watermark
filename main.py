40a2e242dbb64a3689f8f8cfc2247591384d9fbc
arturbednarz <slliver@icloud.com>
2024-04-03 16:42:57 +0200

    Apply main program function. The most basic works on 1 img. No preview

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import tkinter
-from pillow import PillowConverter
 from ui import Interface
 
 
+# TODO Multiple files conversion
 def main():
-    ui = Interface()
-    pillow = PillowConverter()
+    ui = Interface(water_text="@slliver")
+    #ui.create_watermark("KOTSEKTOwdeqd1$#@$@$")
 
 
 if __name__ == '__main__':
--- ui.py
+++ ui.py
@@ -1,12 +1,18 @@
 import tkinter as tk
 from tkinter import filedialog
-from PIL import ImageTk, Image
+from PIL import ImageTk
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 THEME_COLOR = "#375362"
 
 
 class Interface:
-    def __init__(self):
+    def __init__(self, water_text=""):
+        self.images_paths = None
+        self.watermark_text = water_text
+
         self.window = tk.Tk()
         self.window.title("Watermark Adder")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -20,18 +26,39 @@ class Interface:
         self.status_label = tk.Label(self.window, text="", padx=20, pady=10, bg=THEME_COLOR)
         self.status_label.pack()
 
+        self.convert_button = tk.Button(self.window, text="Convert", command=self.create_watermark,
+                                        highlightbackground=THEME_COLOR)
+        self.convert_button.pack(padx=20, pady=10)
+
         self.window.mainloop()
 
-    def display_image(self, filename):
-        image = Image.open(filename)
+    def display_images(self):
+        image = Image.open(self.images_paths)
         photo = ImageTk.PhotoImage(image)
-        self.image_label.config(image=photo)
+        self.image_label.config(image=photo, height=400)
         self.image_label.photo = photo
-        self.status_label.config(text=f"Image loaded: {filename}")
+        self.status_label.config(text=f"Image loaded: {self.images_paths}")
 
     def upload_action(self):
         f_types = [('Jpg Files', '*.jpg'), ('PNG Files', '*.png')]
         filename = filedialog.askopenfilename(filetypes=f_types)
 
         if filename:
-            self.display_image(filename)
+            self.images_paths = filename
+            self.display_images()
+
+    def create_watermark(self):
+        try:
+            img = Image.open(self.images_paths)
+            draw = ImageDraw.Draw(img)
+            # font = ImageFont.truetype(<font-file>, <font-size>)
+            font = ImageFont.truetype("sans-serif.ttf", 16)
+            # draw.text((x, y),"Sample Text",(r,g,b))
+            draw.text((0, 0), f"{self.watermark_text}", (255, 255, 255), font=font)
+            img = img.convert('RGB')
+            img.save('sample-out.jpg')
+            self.status_label.config(text="Watermark successfully added!")
+            #self.display_images()
+        except Exception as e:
+            print(f"There was error: {e}")
+            self.status_label.config(text="Error!")
